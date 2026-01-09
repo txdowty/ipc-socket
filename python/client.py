9279d8376447369249a083d584bde8a262289413
Tim Dowty <tdowty@innoflight.com>
2026-01-09 15:58:04 -0800

    more work

--- python/client.py
+++ python/client.py
@@ -1,18 +1,10 @@
 
 import json
 import socket
-import time
-
-# class Serializable:
-#     def __init__(self):
-#         pass
-#     def deserialize(self):
-#         raise NotImplementedError
-#     def serialize(self):
-#         raise NotImplementedError
 
 class MyStruct:
-    def __init__(self, big, small):
+    def __init__(self, command, big, small):
+        self.cmd = command
         self.big = big
         self.small = small
 
@@ -26,7 +18,7 @@ class MyStruct:
         return json.dumps(self.__dict__, separators=(',', ':'))
  
 
-my_struct = MyStruct(128, 64)
+my_struct = MyStruct('do-it', 128, 64)
 
 def client_program():
     # Define the server's host and port
@@ -37,7 +29,7 @@ def client_program():
     # time.sleep(1)
     # Create a socket object using IPv4 (AF_INET) and TCP (SOCK_STREAM)
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_sock:
             # Connect to the server
             client_sock.connect((host, port))
 
@@ -45,27 +37,24 @@ def client_program():
             size = len(json_str)
             print(f"Sending: {json_str!r}")
 
-            # formatted_hex = ' '.join(f'{b:02x}' for b in json_str)
-            # print(formatted_hex)
-
             # Send data to the server, encoding the string to bytes
-            client_sock.sendall(size.to_bytes(4, byteorder='big'))  # Send size first
+            # client_sock.sendall(size.to_bytes(4, byteorder='big'))  # Send size first
             client_sock.sendall(json_str.encode('utf-8'))
             
             # Shut down the sending side to signal the server that we are done sending data
             client_sock.shutdown(socket.SHUT_WR)
 
             chunks = []
-            while True:
-                # Receive data from the server (buffer size 1024 bytes)
-                data = client_sock.recv(1024)
-                if not data:
-                    # If recv returns an empty bytes object, the server closed the connection
-                    break
-                chunks.append(data)
+            # while True:
+            #     # Receive data from the server (buffer size 1024 bytes)
+            #     data = client_sock.recv(1024)
+            #     if not data:
+            #         # If recv returns an empty bytes object, the server closed the connection
+            #         break
+            #     chunks.append(data)
             
-            received_message = b''.join(chunks).decode()
-            print(f"Received: {received_message!r}")
+            # received_message = b''.join(chunks).decode()
+            # print(f"Received: {received_message!r}")
 
         # The 'with' statement ensures the socket is automatically closed
         print("Connection closed")
--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,89 @@
-use std::{
-    io::{self, BufReader, ErrorKind, prelude::*},
-    mem,
-    net::{TcpListener, TcpStream}
-};
-    use std::net::SocketAddrV4;
+//! IpcServer
+//! 
+//! Provides a server on a dgram internet socket
+//!
+ 
+#![allow(dead_code)]
 
-pub struct Connection {
-    sock_addr: SocketAddrV4,
-    listener: Option<TcpListener>,
+use std::{
+    io::{self, ErrorKind},
+    net::{SocketAddrV4, UdpSocket}
+};
+
+struct IpcServer {
+    addr: SocketAddrV4,
+    socket: Option<UdpSocket>,
 }
 
-impl Connection {
+impl IpcServer {
+
+    /// Creates an IpcServer instance.
+    /// 
+    /// # Arguments
+    /// * `sock_addr` - The server address as a SocketAddrV4.
+    /// 
+    /// # Returns
+    /// The new object instance.
+    /// 
     pub fn new(sock_addr: SocketAddrV4) -> Self {
-        Connection {
-            sock_addr: sock_addr,
-            listener: None,
+        IpcServer {
+            addr: sock_addr,
+            socket: None,
         }
     }
 
+    /// Binds to the address used to construct the object.
+    /// 
+    /// # Arguments
+    /// * none
+    /// 
+    /// # Returns
     pub fn connect(&mut self) -> io::Result<()> {
-        let listener = TcpListener::bind(self.sock_addr)?;
-        // set non-blocking
-        listener.set_nonblocking(true)?;
-        self.listener = Some(listener);
+        let socket = UdpSocket::bind(self.addr)?;
+        socket.set_nonblocking(true).unwrap();
+        self.socket = Some(socket);
+
         Ok(())
     }
 
-    pub fn get_data(&self, max_size: usize) -> io::Result<Vec<u8>> {
-        // if self.listener.is_none() {
-        //     return None;
-        // }
-
-        let incoming = self.listener.as_ref().unwrap().incoming();
-        for stream in incoming {
-            match stream {
-                Ok(stream) => {
-                    return Self::handle_connection(stream, max_size);
+    pub fn get_data(&self, max_size: usize) -> io::Result<Option<Vec<u8>>> {
+        let mut buffer: Vec<u8> = vec![0; max_size];
+        let socket = self.get_socket().unwrap();
+        match socket.recv_from(buffer.as_mut_slice()) {
+            Ok((0, _)) => return Ok(None),
+            Ok((len, _)) => {
+                buffer.truncate(len);
+                return Ok(Some(buffer));
+            },
+            Err(e) => {
+                if !matches!(e.kind(), ErrorKind::WouldBlock) {
+                    return  Err(e);
                 }
-                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                    // No incoming connection, return empty vec
-                    break;
-                }
-                Err(e) => {
-                    return Err(e);
-                }
-            }
+            },
         }
-        Ok(Vec::new())
+        Ok(None)
     }
 
-    fn get_message_size(stream: &mut BufReader<&TcpStream>) -> io::Result<usize> {
-        let mut size_buffer = [0u8; 4];
-        stream.read_exact(&mut size_buffer)?;
-        let size = u32::from_be_bytes(size_buffer) as usize;
-        Ok(size)
+    // ---- private -----
+    fn get_socket(&self) -> Option<&UdpSocket> {
+        self.socket.as_ref()
     }
 
-    fn handle_connection(stream: TcpStream, max_size: usize) -> io::Result<Vec<u8>> {
-        let mut buf_reader = BufReader::new(&stream);
 
-        // get message size from message header
-        let total_size = Self::get_message_size(&mut buf_reader).unwrap();
-
-        // is max_size adequate?
-        if total_size + mem::size_of::<u32>() > max_size {
-            return Err(io::Error::new(ErrorKind::FileTooLarge, "max size exceeded"));
-        }
-
-        // allocate data buffer
-        let mut buffer: Vec<u8> = vec![0; total_size];
-        // let buffer_slice: &mut [u8] = &mut buffer[..];
-        let buffer_slice: &mut [u8] = buffer.as_mut_slice();
-
-        buf_reader.read_exact(buffer_slice)?;
-
-        Ok(buffer)
-    }
-}
+ }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // use serde_cbor::from_slice;
     use serde::{Deserialize, Serialize};
-    use std::path::{Path, PathBuf};
-    use std::process::{Child, Command, Stdio};
-    use std::str::FromStr;
     use std::{env};
+    use std::io::{BufRead, BufReader};
     use std::net::SocketAddrV4;
+    use std::path::{Path, PathBuf};
+    use std::process::{Child, Command, Stdio, ChildStdout};
+    use std::str::FromStr;
+    use serde_json::{Value};
 
     #[derive(Deserialize, Serialize, PartialEq, Eq, Debug)]
     struct MyStruct {
@@ -113,6 +108,15 @@ mod tests {
         script_cmd.spawn()
     }
 
+    fn print_bufreader(reader: BufReader<ChildStdout>) {
+       for line in reader.lines() {
+            match line {
+                Ok(line) => println!("Child Output: {}", line),
+                Err(e) => eprintln!("Error reading line: {}", e),
+            }
+        }
+    }
+
     #[test]
     fn basic() {
 
@@ -124,47 +128,56 @@ mod tests {
         let endpoint = "127.0.0.1:12345";
         let socket_addr = SocketAddrV4::from_str(endpoint);
 
-        let mut connection: Connection = Connection::new(socket_addr.unwrap());
+        let mut connection: IpcServer = IpcServer::new(socket_addr.unwrap());
         match connection.connect() {
             Err(e) => assert!(false, "{}", e),
             Ok(()) => {}
         }
 
-        // Take the stdout handle from the child and wrap it in a BufReader
         let mut child_handle = spawn_python_script(Path::new("python/client.py")).unwrap();
+        // Take the stdout handle from the child and wrap it in a BufReader
         let child_stdout = child_handle.stdout.take().expect("Could not capture stdout");
-        let reader = BufReader::new(child_stdout);
 
         loop {
             std::thread::sleep(std::time::Duration::from_millis(100));
-            // let x = connection.get_data(&mut buffer);
+  
             match connection.get_data(512) {
+                Ok(None) => continue, // no data available
+                Ok(buffer) => {
+                    let unwrapped_buf = buffer.unwrap();
+                    let len = unwrapped_buf.len();
+                    if len > 0 {
+                        println!("Received data size: {:?}", len);
+                        let json_str = String::from_utf8(unwrapped_buf)
+                        .unwrap();
+                    
+                        println!("Received data: {}", json_str);
+                        let _d: Value = serde_json::from_str(json_str.as_str()).unwrap();
+                        println!("{:#?}", &_d);
+                        if let Value::String(s) = &_d["cmd"] {
+                            match &s[..] {
+                                "do-it" => {
+                                    let deserialized_obj: MyStruct = serde_json::from_str(json_str.as_str()).unwrap();
+                                    assert_eq!(deserialized_obj.big, my_struct.big);
+                                    assert_eq!(deserialized_obj.small, my_struct.small);
+                                    break;
+                                    
+                                },
+                                _ => assert!(false, "Unknown cmd: {}", s)
+                            }
+                        }
+                    }
+                }
                 Err(e) => {
                     assert!(false, "{}", e);
                 }
-                Ok(buffer) => {
-                    if buffer.len() > 0 {
-                        println!("Received data size: {:?}", buffer.len());
-                        let json_str = String::from_utf8(buffer)
-                                                    .unwrap();
-                        // deserialize from JSON format
-                        println!("Received data: {:?}", json_str);
-                        let deserialized_obj: MyStruct = serde_json::from_str(json_str.as_str()).unwrap();
-                        assert_eq!(deserialized_obj.big, my_struct.big);
-                        assert_eq!(deserialized_obj.small, my_struct.small);
-                        break;
-                    }
-                }
             }
         }
-        // Read and print the output line by line
-        println!("Reading output from child process:");
-        for line in reader.lines() {
-            match line {
-                Ok(line) => println!("Child Output: {}", line),
-                Err(e) => eprintln!("Error reading line: {}", e),
-            }
-        }
-    }
 
+        // Read and print the python script stdout
+        println!("Reading output from child process:");
+        let buf_reader = BufReader::new(child_stdout);
+        print_bufreader(buf_reader);
+    }
 }
+
